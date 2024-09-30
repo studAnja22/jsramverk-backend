fcd951238b2a8aa749e29910eb4150529f456175
Anna <anja22@student.bth.se>
2024-09-30 17:33:40 +0200

    refactoring routes so they work with frontend

--- routes/posts.mjs
+++ routes/posts.mjs
@@ -2,36 +2,37 @@ import express from 'express';
 const router = express.Router();
 
 import documents from "../models/docs.mjs";
+// Get all documents
+router.get('/', async (req, res) => {
+    const docs = await documents.getAll();
+
+    return res.json(docs);
+});
+
 // Adds a new document to the database
 router.post("/", async (req, res) => {
     const result = await documents.addOne(req.body);
     const documentId = result.insertedId.toString();
 
-    return res.redirect(`/posts/${documentId}`);
+    return res.redirect(`/${documentId}`);
+    // return res.redirect(`/posts/${documentId}`);// for postman
 });
+
+// Get one document
+router.get('/:id', async (req, res) => {
+    const docs = await documents.getOne(req.params.id);
+
+    return res.json(docs);
+});
+
 // updates an existing document in the database
 router.post("/update", async (req, res) => {
     await documents.updateOne(req.body);
 
     const documentId = req.body["_id"];
 
-    return res.redirect(`/posts/${documentId}`);
-});
-
-router.get('/:id', async (req, res) => {
-    const docs = await documents.getOne(req.params.id);
-
-    return res.json({
-        data: docs
-    });
-});
-
-router.get('/', async (req, res) => {
-    const docs = await documents.getAll();
-
-    return res.json({
-        data: docs
-    });
+    return res.redirect(`/${documentId}`);
+    // return res.redirect(`/posts/${documentId}`);// for postman
 });
 
 export default router;
