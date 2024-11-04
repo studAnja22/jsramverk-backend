e7a8160a8ac3f3c9f136b6e314fef42d078da2a7
Anna <anja22@student.bth.se>
2024-11-04 15:07:57 +0100

    auth.logout feature

--- models/auth.mjs
+++ models/auth.mjs
@@ -164,6 +164,10 @@ const auth = {
             console.error("No token found.")
             return false;
         }
+    },
+    logout: function logout() {
+        auth.token = "";
+        auth.user = "";
     }
 }
 
